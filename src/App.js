f4cea486fd62d072d4a90a637f9541f517cfca44
gksrufaos <gksrufaos@gmail.com>
2019-11-01 16:25:26 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import Potato from './Potato';
+
+function Food({fav}){
+  return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
     <div className="App">
       <h1>hello!</h1>
-      <Potato />
+      <Food fav="kimchi"/>
+      <Food fav="chicken"/>
+      <Food fav="ramen"/>
     </div>
   );
 }
 
 export default App;
+
+/*
+  컴포넌트에 property(props)에 value를 넣어서 Food의 컴포넌트로 넘김         
+*/
--- src/Potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Potato(){
-    return <h3>I Love potato</h3>;
-}
-
-export default Potato;
