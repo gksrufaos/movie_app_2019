8529651be74ab477c75030f62c2d4c2798c06feb
gksrufaos <gksrufaos@gmail.com>
2019-11-01 16:33:23 +0900

    #2.3 .map Recap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
 
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:"http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    id: 2,
+    name: "Doncasu",
+    image:"https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    id: 3,
+    name: "Kimbap",
+    image:"http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
+
 function Food({name, picture}){
   return (
     <div>
@@ -9,26 +27,11 @@ function Food({name, picture}){
   )
 }
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:"http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    name: "Doncasu",
-    image:"https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:"http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-];
-
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name = {dish.name} picture = {dish.image}/>
+        <Food key= {dish.id} name = {dish.name} picture = {dish.image}/>
       ))}            
     </div>
   );
