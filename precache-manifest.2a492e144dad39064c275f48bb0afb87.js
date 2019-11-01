1283e941a9457e9e7d52d9239e0610f19f20fe0c
gksrufaos <gksrufaos@gmail.com>
2019-11-01 18:44:01 +0900

    Updates

---
+++ precache-manifest.2a492e144dad39064c275f48bb0afb87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4111f34c19cffea018588ea68af447f",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "2d079422a3a96cbd9d67",
+    "url": "/movie_app_2019/static/css/main.1b7b6bb5.chunk.css"
+  },
+  {
+    "revision": "2bed164abecb2001a4f8",
+    "url": "/movie_app_2019/static/js/2.14ba9984.chunk.js"
+  },
+  {
+    "revision": "2d079422a3a96cbd9d67",
+    "url": "/movie_app_2019/static/js/main.a5bb798a.chunk.js"
+  },
+  {
+    "revision": "44901d82ce6105999ea4",
+    "url": "/movie_app_2019/static/js/runtime-main.0f07e758.js"
+  }
+]);
